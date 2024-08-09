8e3266521f07a5d8a5ad897ce78cc13a29e96501
Kalvine <kkalvine4@gmail.com>
2024-08-09 05:15:40 +0300

    Writes tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,31 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", ()=> {
+    it("returns TRUE for a word that is read the same front and backwards", ()=> {
+        const word = "racecar"
+
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(true)
+    });
+
+    it("returns TRUE for a word that is read the same front and backwards regardless of case", ()=> {
+        const word = "raCecAr"
+
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(true)
+    });
+
+    it("throws an error if non-alphabetic characters are used", ()=> {
+        const word = "12345"
+
+        expect(()=> isPalindrome(word).toThrow())
+    });
+
+    it("throws an error if empty strings are used", ()=> {
+        const word = ""
+
+        expect(()=> isPalindrome(word).toThrow())
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+export function isPalindrome(word){
+
+    const newWord = word.toLowerCase()
+    
+    if ((newWord.split('').reverse().join('')) === newWord){
+        return true;
+    }else if (!/^[A-Za-z]+$/.test(word)){
+        throw new Error ("Avoid spaces and special characters");
+    }
+}
